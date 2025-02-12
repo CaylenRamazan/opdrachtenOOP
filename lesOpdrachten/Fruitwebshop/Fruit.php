72bc4fba303067278af650b86139f1e0b293c909
CaylenRamazan <9023381@student.zadkine.nl>
2025-02-12 09:13:58 +0100

    Initial commit

---
+++ lesOpdrachten/Fruitwebshop/Fruit.php
@@ -0,0 +1,17 @@
+<?php
+// functie definitie class Fruit
+// auteur Caylen Ramazan
+
+class Fruit{
+
+    // properties
+    public $Name;
+
+    // functions
+    function printName(){
+
+    }
+
+}
+
+?>
---
+++ lesOpdrachten/Fruitwebshop/webshopFruit.php
@@ -0,0 +1,18 @@
+<?php
+// Functie: hoofdprogramma webshop fruit
+// Auteur: Caylen Ramazan
+
+
+// Initialisatie
+include_once "functions.php";
+include_once "Fruit.php";
+
+// Main
+
+
+// Aanmaken object op basis van de class beschrijving Fruit
+$banaan = new Fruit();
+var_dump($banaan);
+
+
+?>
